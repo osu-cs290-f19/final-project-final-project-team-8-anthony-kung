e41f6f08c2998b59f98c1ff7acaccf22f0bdc2a6
Anthony Kung <chiefanthony@hailiga.org>
2019-12-07 22:49:55 -0800

    Update from ENGR server

--- program/html/assets/navBar.js
+++ program/html/assets/navBar.js
@@ -1,9 +1,25 @@
 var navBar = document.getElementsByClassName("navitems");
 
+console.log(navBar);
+console.log(navBar.length);
+
+console.log(document.getElementsByClassName("navitems")[0]);
+console.log(navBar[1].href);
+
 for (var i = 0; i < navBar.length; i++) {
-  if (navBar[i].href == window.location.pathname)
+  if (navBar[i].href == window.location.href) {
     navBar[i].classList.add("active");
+  }
 }
 
 console.log(navBar);
 console.log(navBar.length);
+
+function navBarResClick() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbarctn") {
+    x.className += " resnav";
+  } else {
+    x.className = "navbarctn";
+  }
+}
